b5f3ad1c40110e2de67732d733ef721aad859f06
Kyle Talmage <ktalmage85@gmail.com>
2020-02-17 19:29:01 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,11 +3,3 @@
 
 def key_for_min_value(name_hash)
 
-name_hash.values.flatten
-
-if name
-
-return name_array
-end
- name_hash = { :one => 1, :two => 2, :three => 3}
- puts key_for_min_value(name_hash)
