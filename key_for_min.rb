eaa5ee036c69166898da0d99e82dd5f18ba2898b
Kyle Talmage <ktalmage85@gmail.com>
2020-02-17 19:34:53 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -9,12 +9,12 @@ smallest_value = nil
 
 name_hash.each do |key,value|
   if smallest_value == nil || value < smallest_value
-    binding.pry
+    
     smallest_value = value
     smallest_key = key
     end
   end
   smallest_key
 end
-
+  
   
